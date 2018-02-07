8312fe4334ffb2e6194d7fa9072b1fa765ae1106
Stjepan Mudronja <smudronja.sm@gmail.com>
2018-02-07 19:23:18 +0100

    finished search field

--- article.php
+++ article.php
@@ -2,17 +2,15 @@
 	include 'header.php';
  ?>
 	
-	<form action="search.php" method="POST">
-		<input type="text" name="search" placeholder="Search">
-		<button type="submit" name="submit-search">Search</button>
-	</form>
-
-	<h1>Front page</h1>
-	<h2>All articles:</h2>
+	<h1>Article page</h1>
+	
 
 	<div class="article-container">
 		<?php 
-			$sql = "SELECT * FROM article";
+		$title = mysqli_real_escape_string($conn, $_GET['title']);
+		$date = mysqli_real_escape_string($conn, $_GET['date']);
+
+			$sql = "SELECT * FROM article WHERE a_title='$title' AND a_date = '$date'";
 			$result = mysqli_query($conn, $sql);
 			$queryResults = mysqli_num_rows($result);
 
